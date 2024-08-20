f21f3ee543ba588cb037874a18cfb90c8a8a41a9
Alex Canario <alexcanario@gmail.com>
2024-08-19 22:09:32 -0300

    feat[project]: Reset project structure to clean archicteture

--- .sonarqube/out/0/output-cs/file-metadata.pb
+++
@@ -1,4 +0,0 @@
-^
-SD:\labs\_cs\Hydros\src\Hydros.Infra\obj\Debug\net8.0\Hydros.Infra.GlobalUsings.g.csutf-8n
-cD:\labs\_cs\Hydros\src\Hydros.Infra\obj\Debug\net8.0\.NETCoreApp,Version=v8.0.AssemblyAttributes.csutf-8\
-QD:\labs\_cs\Hydros\src\Hydros.Infra\obj\Debug\net8.0\Hydros.Infra.AssemblyInfo.csutf-8
--- src/Hydros.Api/Program.cs
+++ src/Hydros.Api/Program.cs
@@ -1,36 +1,32 @@
+using Hydros.App;
+using Hydros.Infra;
+using Hydros.Presentation;
 
-namespace Hydros.Api
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, config) =>
+    config.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-
-			// Add services to the container.
-
-			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
-			builder.Services.AddSwaggerGen();
-
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
-
-			app.UseHttpsRedirection();
-
-			app.UseAuthorization();
-
-
-			app.MapControllers();
-
-			app.Run();
-		}
-	}
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
